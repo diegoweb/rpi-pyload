43752196dc52f6697f1fa3dc7d9bc32fa63ad114
diegoweb <diegoweb@live.com>
2018-09-11 17:41:58 -0300

    Update start_pyload.sh

--- start_pyload.sh
+++ start_pyload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 8001:8000 -p 7227:7227 -v /opt/pyload/downloads:/opt/pyload/Downloads -v /opt/pyload/config:/opt/pyload/pyload-config --name pyload dastrasmue/rpi-pyload
+docker run -d -p 8000:8000 -p 7227:7227 -e UID=1000 -e GID=1000 -v /home/pi/downloads:/opt/pyload/Downloads -v /home/pi/pyload-config:/opt/pyload/pyload-config --name pyload diegoweb/rpi-pyload
