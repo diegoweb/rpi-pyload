cd594487f281705749b7206e721dbd46f4f3a16b
diegoweb <diegoweb@live.com>
2018-09-12 10:07:17 -0300

    Delete start_pyload.sh

--- start_pyload.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d -p 8000:8000 -p 7227:7227 -e UID=1000 -e GID=1000 -v /home/pi/downloads:/opt/pyload/Downloads -v /home/pi/pyload-config:/opt/pyload/pyload-config --name pyload diegoweb/rpi-pyload
